149a9c6af807a85e1d559a38b9495ddbf2a5f4dc
Shuffle10 <saphal.pant@westcliff.edu>
2024-01-12 02:21:28 -0600

    Updated UI

--- Game.py
+++ Game.py
@@ -1,33 +1,63 @@
 from tkinter import *
 from CardGame import *
+from time import *
 
-class App(Frame):
+FONTSTYLE = ('Helvetica 16')
+BGCOLOR = '#232d3d'
+
+class Game(Frame):
     
     def __init__(self, master):
         Frame.__init__(self, master)
+        self.deck = self.generateDeck()
         self.master = master
-        self.label_1 = Label(self.master, text='Computer Picked')
-        self.label_1.grid(row=0, column=0, sticky=W, padx=5)
-        self.label_2 = Label(self.master, text='User Picked')
-        self.label_2.grid(row=0, column=1, sticky=E)
+        self.label_1 = Label(self.master, text='Computer Picked', font=FONTSTYLE, background=BGCOLOR, fg='white', pady=10)
+        self.label_1.grid(row=0, column=0, sticky=N+S+E+W)
+        self.label_2 = Label(self.master, text='User Picked', font=FONTSTYLE, background=BGCOLOR, fg='white', pady=10)
+        self.label_2.grid(row=0, column=1, sticky=N+S+E+W)
         self.comp_image = PhotoImage(file="images/default.png")
-        self.comp_image_label = Label(self.master, image=self.comp_image, height="726", width="500")
+        self.comp_image_label = Label(self.master, image=self.comp_image, height="726", width="500", background=BGCOLOR, padx=5)
         self.comp_image_label.image = self.comp_image
         self.comp_image_label.grid(column=0, row=1)
         self.user_image = PhotoImage(file="images/default.png")
-        self.user_image_label = Label(self.master, image=self.user_image, height="726", width="500")
+        self.user_image_label = Label(self.master, image=self.user_image, height="726", width="500", background=BGCOLOR, padx=5)
         self.user_image_label.image = self.user_image
         self.user_image_label.grid(column=1, row=1)
-        self.winner_label = Label(self.master, text='')
+        self.winner_label = Label(self.master, text=f"Remaining Cards: {self.deck.size()}", font=FONTSTYLE, background=BGCOLOR, fg='white', pady=12)
         self.winner_label.grid(row=2, column=0, columnspan=2, sticky=N+S+E+W)
-        self.btn_wrapper = Label(self.master)
-        self.play_btn = Button(self.master, text='Play', command=self.play)
-        self.play_btn.grid(row=3, column=0, sticky=W)
-        self.restart_btn = Button(self.master, text='Restart', command=self.restart)
-        self.restart_btn.grid(row=3, column=0, sticky=E)
-        self.quit_btn = Button(self.master, text='Quit', command=self.quit)
-        self.quit_btn.grid(row=3, column=1, sticky=E)
-        self.deck = self.generateDeck()
+        self.play_btn = Button(
+            self.master,
+            width=12,
+            text='Play',
+            command=self.play,
+            bg='#ffae00',  
+            fg='#FFFFFF',  
+            font=('Helvetica', 12),  
+            relief='flat', 
+        )
+        self.play_btn.grid(row=3, column=0, sticky=W, padx=10, pady=10)
+        self.restart_btn = Button(
+            self.master,
+            width=12,
+            text='Restart',
+            command=self.restart,
+            bg='#ffae00',  
+            fg='#FFFFFF',  
+            font=('Helvetica', 12),
+            relief='flat',   
+        )
+        self.restart_btn.grid(row=3, column=1, pady=10)
+        self.quit_btn = Button(
+            self.master,
+            width=12,
+            text='Quit',
+            command=self.quit,
+            bg='#ffae00',  
+            fg='#FFFFFF',  
+            font=('Helvetica', 12),
+            relief='flat',   
+        )
+        self.quit_btn.grid(row=3, column=1, sticky=E, padx=10, pady=10)
         
     @property
     def deck(self):
@@ -40,16 +70,11 @@ class App(Frame):
     def generateDeck(self):
         return Deck()   
         
-    def play(self):
-        print(self.deck)
-        
     def restart(self):
         self.deck = self.generateDeck()
+        self.winner_label["text"] = f"Remaining Cards: {self.deck.size()}"
         self.user_image["file"] = "images/default.png"
         self.comp_image["file"] = "images/default.png"
-          
-    def end(self):
-        pass
 
     #game logic
     def play(self):
@@ -64,16 +89,18 @@ class App(Frame):
                 self.comp_image["file"] = f'images/{computer.imagefile}'
                 #comparison of the cards
                 if (user>computer):
-                    self.winner_label["text"]="YOU WIN"
+                    self.winner_label["text"]=f"Remaining Cards: {self.deck.size()}\nYOU WIN"
                 elif (computer>user):
-                    self.winner_label["text"]="I WIN"
+                    self.winner_label["text"]=f"Remaining Cards: {self.deck.size()}\nI WIN"
                 else:
-                    self.winner_label["text"]="DRAW"
+                    self.winner_label["text"]=f"Remaining Cards: {self.deck.size()}\nDRAW"
             else:
-                self.winner_label["text"]="Not enogh cards to play!"
-                self.end()
-
+                self.quit()
+                
 
 window = Tk()
-app = App(window)
+window.title("Highest Wins")
+window.resizable(False, False)
+window.configure(background=BGCOLOR)
+app = WelcomeWindow(window)
 window.mainloop()
